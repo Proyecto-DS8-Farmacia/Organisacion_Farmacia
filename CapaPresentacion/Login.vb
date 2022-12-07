2557ae2f31d1c40e555f643bbf2863214fe2bd70
guillermo-gordon18-2000 <guillermo18zgordon@gmail.com>
2022-12-06 20:59:54 -0500

    "NJH-8989"

---
+++ CapaPresentacion/Login.vb
@@ -0,0 +1,47 @@
+﻿Imports System.Text.RegularExpressions
+Imports System.Data.SqlClient
+Public Class Login
+    Private Sub Login_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+
+
+
+    End Sub
+
+    Private Sub btnIngresar_Click(sender As Object, e As EventArgs) Handles btnIngresar.Click
+        Dim regex As Regex = New Regex("^[^@\s]+@[^@\s]+\.[^@\s]+$")
+        Dim validocorreo As Boolean = regex.IsMatch(txtCorreo.Text.Trim)
+
+
+        If (txtCorreo.Text.Equals("") Or txtContraseña.Text.Equals("")) Then
+            MessageBox.Show("Ingrese sus datos correctamente.", "Aviso!!", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Else
+            If Not validocorreo Then
+                MessageBox.Show("Ingrese un correo valido.", "Aviso!!", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Else
+
+
+                If (CapaDatos.Metodos.INICIAR_SESION(txtCorreo.Text.ToString, txtContraseña.Text.ToString, "Doctor")) Then
+
+
+                    MessageBox.Show("ERES UN DOCTOR", "Aviso!!", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                    Panel.Show()
+                    Me.Hide()
+
+
+                Else
+                    MessageBox.Show("No se inicio sesion. Revise sus credenciales.", "Aviso!!", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                End If
+
+
+            End If
+
+
+        End If
+        '' txtCorreo.Text = ""
+        txtContraseña.Text = ""
+    End Sub
+
+
+End Class
---
+++ CapaPresentacion/Reporte.Designer.vb
@@ -0,0 +1,286 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Reporte
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnBuscar = New System.Windows.Forms.Button()
+        Me.txtCedula = New System.Windows.Forms.TextBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.DataGridView2 = New System.Windows.Forms.DataGridView()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblEvolucion = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblFIngreso = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lblHIngreso = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.lblNCama = New System.Windows.Forms.Label()
+        Me.lblUM = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.lblDIngreso = New System.Windows.Forms.Label()
+        Me.DataGridView3 = New System.Windows.Forms.DataGridView()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnBuscar
+        '
+        Me.btnBuscar.Location = New System.Drawing.Point(667, 52)
+        Me.btnBuscar.Name = "btnBuscar"
+        Me.btnBuscar.Size = New System.Drawing.Size(94, 29)
+        Me.btnBuscar.TabIndex = 0
+        Me.btnBuscar.Text = "Buscar"
+        Me.btnBuscar.UseVisualStyleBackColor = True
+        '
+        'txtCedula
+        '
+        Me.txtCedula.Location = New System.Drawing.Point(109, 54)
+        Me.txtCedula.Name = "txtCedula"
+        Me.txtCedula.Size = New System.Drawing.Size(500, 27)
+        Me.txtCedula.TabIndex = 1
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(33, 128)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 51
+        Me.DataGridView1.RowTemplate.Height = 29
+        Me.DataGridView1.Size = New System.Drawing.Size(576, 188)
+        Me.DataGridView1.TabIndex = 2
+        '
+        'DataGridView2
+        '
+        Me.DataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView2.Location = New System.Drawing.Point(667, 128)
+        Me.DataGridView2.Name = "DataGridView2"
+        Me.DataGridView2.RowHeadersWidth = 51
+        Me.DataGridView2.RowTemplate.Height = 29
+        Me.DataGridView2.Size = New System.Drawing.Size(578, 188)
+        Me.DataGridView2.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(245, 22)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(229, 20)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Introduzca la cédula del paciente"
+        '
+        'lblEvolucion
+        '
+        Me.lblEvolucion.AutoSize = True
+        Me.lblEvolucion.Location = New System.Drawing.Point(212, 558)
+        Me.lblEvolucion.Name = "lblEvolucion"
+        Me.lblEvolucion.Size = New System.Drawing.Size(53, 20)
+        Me.lblEvolucion.TabIndex = 5
+        Me.lblEvolucion.Text = "Label2"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(23, 371)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(124, 20)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Fecha de ingreso:"
+        '
+        'lblFIngreso
+        '
+        Me.lblFIngreso.AutoSize = True
+        Me.lblFIngreso.Location = New System.Drawing.Point(150, 371)
+        Me.lblFIngreso.Name = "lblFIngreso"
+        Me.lblFIngreso.Size = New System.Drawing.Size(53, 20)
+        Me.lblFIngreso.TabIndex = 7
+        Me.lblFIngreso.Text = "Label3"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(23, 407)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(119, 20)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Hora de ingreso:"
+        '
+        'lblHIngreso
+        '
+        Me.lblHIngreso.AutoSize = True
+        Me.lblHIngreso.Location = New System.Drawing.Point(151, 407)
+        Me.lblHIngreso.Name = "lblHIngreso"
+        Me.lblHIngreso.Size = New System.Drawing.Size(53, 20)
+        Me.lblHIngreso.TabIndex = 9
+        Me.lblHIngreso.Text = "Label5"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(23, 443)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(127, 20)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "Numero de cama:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(23, 480)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(113, 20)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "Unidad medica:"
+        '
+        'lblNCama
+        '
+        Me.lblNCama.AutoSize = True
+        Me.lblNCama.Location = New System.Drawing.Point(150, 443)
+        Me.lblNCama.Name = "lblNCama"
+        Me.lblNCama.Size = New System.Drawing.Size(53, 20)
+        Me.lblNCama.TabIndex = 12
+        Me.lblNCama.Text = "Label6"
+        '
+        'lblUM
+        '
+        Me.lblUM.AutoSize = True
+        Me.lblUM.Location = New System.Drawing.Point(151, 480)
+        Me.lblUM.Name = "lblUM"
+        Me.lblUM.Size = New System.Drawing.Size(53, 20)
+        Me.lblUM.TabIndex = 13
+        Me.lblUM.Text = "Label7"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(23, 600)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(166, 20)
+        Me.Label6.TabIndex = 14
+        Me.Label6.Text = "Diagnostico de ingreso:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(23, 558)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(148, 20)
+        Me.Label7.TabIndex = 15
+        Me.Label7.Text = "Notas de enfermería:"
+        '
+        'lblDIngreso
+        '
+        Me.lblDIngreso.AutoSize = True
+        Me.lblDIngreso.Location = New System.Drawing.Point(212, 600)
+        Me.lblDIngreso.Name = "lblDIngreso"
+        Me.lblDIngreso.Size = New System.Drawing.Size(53, 20)
+        Me.lblDIngreso.TabIndex = 16
+        Me.lblDIngreso.Text = "Label8"
+        '
+        'DataGridView3
+        '
+        Me.DataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView3.Location = New System.Drawing.Point(347, 371)
+        Me.DataGridView3.Name = "DataGridView3"
+        Me.DataGridView3.RowHeadersWidth = 51
+        Me.DataGridView3.RowTemplate.Height = 29
+        Me.DataGridView3.Size = New System.Drawing.Size(586, 141)
+        Me.DataGridView3.TabIndex = 17
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(616, 95)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(53, 20)
+        Me.Label8.TabIndex = 18
+        Me.Label8.Text = "Label8"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(616, 335)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(53, 20)
+        Me.Label9.TabIndex = 19
+        Me.Label9.Text = "Label9"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1280, 656)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.DataGridView3)
+        Me.Controls.Add(Me.lblDIngreso)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.lblUM)
+        Me.Controls.Add(Me.lblNCama)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.lblHIngreso)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.lblFIngreso)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.lblEvolucion)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.DataGridView2)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.txtCedula)
+        Me.Controls.Add(Me.btnBuscar)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnBuscar As Button
+    Friend WithEvents txtCedula As TextBox
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents DataGridView2 As DataGridView
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblEvolucion As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lblFIngreso As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents lblHIngreso As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents lblNCama As Label
+    Friend WithEvents lblUM As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents lblDIngreso As Label
+    Friend WithEvents DataGridView3 As DataGridView
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+End Class
